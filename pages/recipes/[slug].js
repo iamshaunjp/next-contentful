91b468d5573b85413d22d6697965cbf707bdda75
Shaun <shaun@thenetninja.co.uk>
2021-03-06 13:17:37 +0000

    lesson-7

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,42 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ 
+    content_type: "recipe" 
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: { recipe: items[0] }
+  }
+
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe)
+  
   return (
     <div>
       Recipe Details
