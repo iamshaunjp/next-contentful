0272bdad9864b738cda836704b06c148d32f97e2
Shaun <shaun@thenetninja.co.uk>
2021-03-06 08:04:47 +0000

    lesson-3

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
-export default function Recipes() {
+import { createClient } from 'contentful'
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const res = await client.getEntries({ content_type: "recipe" })
+
+  return {
+    props: {
+      recipes: res.items,
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes)
+
   return (
     <div className="recipe-list">
       Recipe List
