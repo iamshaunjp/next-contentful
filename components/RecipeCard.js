362b2ce1afb85fabdff91813e81201718d722604
Shaun <shaun@thenetninja.co.uk>
2021-03-06 13:10:33 +0000

    lesson-6

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -22,6 +22,42 @@ export default function RecipeCard({ recipe }) {
           <Link href={'/recipes/' + slug}><a>Cook this</a></Link>
         </div>
       </div>
+
+      <style jsx>{`
+        .card {
+          transform: rotateZ(-1deg);
+        }
+        .content {
+          background: #fff;
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+          margin: 0;
+          position: relative;
+          top: -40px;
+          left: -10px;
+        }
+        .info {
+          padding: 16px;
+        }
+        .info h4 {
+          margin: 4px 0;
+          text-transform: uppercase;
+        }
+        .info p {
+          margin: 0;
+          color: #777;
+        }
+        .actions {
+          margin-top: 20px;
+          display: flex;
+          justify-content: flex-end;
+        }
+        .actions a {
+          color: #fff;
+          background: #f01b29;
+          padding: 16px 24px;
+          text-decoration: none;
+        }
+      `}</style>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,14 @@ export default function Recipes({ recipes }) {
       {recipes.map(recipe => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
